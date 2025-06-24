3305390c76f1dad33fbdd41f69587cde8223322b
Fahad Ullah Khan <fahadullahk4@gmail.com>
2025-06-24 19:21:59 +0500

    paused for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { use, useEffect } from "react";
 import Canvas from "./components/Canvas";
 import data from "./data";
+import LocomotiveScroll from "locomotive-scroll";
 
 const App = () => {
+	useEffect(() => {
+		const locomotiveScroll = new LocomotiveScroll();
+	}, []);
 	return (
 		<>
-			<div className="w-full min-h-screen bg-zinc-900 text-white">
-				{data.map((item, index) => (
-					<div key={index}>
-						{item.map((canvasdets, index) => (
-							<Canvas details={canvasdets} />
-						))}
-					</div>
+			<div className="w-full relative min-h-screen bg-zinc-900 text-white">
+				{data[0].map((canvasdets, index) => (
+					<Canvas details={canvasdets} />
+				))}
+			</div>
+			<div className="w-full relative min-h-screen bg-zinc-900 text-white">
+				{data[1].map((canvasdets, index) => (
+					<Canvas details={canvasdets} />
+				))}
+			</div>
+			<div className="w-full relative min-h-screen bg-zinc-900 text-white">
+				{data[2].map((canvasdets, index) => (
+					<Canvas details={canvasdets} />
+				))}
+			</div>
+			<div className="w-full relative min-h-screen bg-zinc-900 text-white">
+				{data[3].map((canvasdets, index) => (
+					<Canvas details={canvasdets} />
 				))}
 			</div>
 		</>
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useRef, useState } from "react";
 import canvasImages from "../canvasImages";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-const Canvas = ({details}) => {
-    const {startIndex, numImages, duration, size, top, left, zIndex} = details;
+const Canvas = ({ details }) => {
+	const { startIndex, numImages, duration, size, top, left, zIndex } = details;
 
 	const [index, setIndex] = useState({ value: startIndex });
 	const canvasRef = useRef(null);
@@ -13,7 +13,7 @@ const Canvas = ({details}) => {
 			value: startIndex + numImages - 1,
 			duration: duration,
 			ease: "linear",
-            repeat: -1,
+			repeat: -1,
 			onUpdate: () => {
 				setIndex({ value: Math.floor(index.value) });
 			},
@@ -22,17 +22,34 @@ const Canvas = ({details}) => {
 
 	// Load the image and draw it on the canvas
 	useEffect(() => {
+		const scale = window.devicePixelRatio;
 		const canvas = canvasRef.current;
 		const ctx = canvas.getContext("2d");
 		const img = new Image();
 		img.src = canvasImages[index.value];
 		img.onload = () => {
-			canvas.width = img.width;
-			canvas.height = img.height;
-			ctx.drawImage(img, 0, 0);
+			canvas.width = canvas.offsetWidth * scale;
+			canvas.height = canvas.offsetHeight * scale;
+			canvas.style.width = canvas.offsetWidth + "px";
+			canvas.style.height = canvas.offsetHeight + "px";
+			ctx.scale(scale, scale);
+			ctx.drawImage(img, 0, 0, canvas.offsetWidth, canvas.offsetHeight);
 		};
 	}, [index]);
-	return <canvas ref={canvasRef} className="size-[{$size}px]" id="canvas"></canvas>;
+	return (
+		<canvas
+        data-scroll data-scroll-speed={Math.random().toFixed(1)}
+			ref={canvasRef}
+			style={{
+				width: `${size * 1.4}px`,
+				height: `${size * 1.4}px`,
+				position: "absolute",
+				top: `${top}%`,
+				left: `${left}%`,
+				zIndex: `${zIndex}`,
+			}}
+			id="canvas"></canvas>
+	);
 };
 
 export default Canvas;
